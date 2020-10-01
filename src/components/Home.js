16e4fc8f904091303464cbd9c81e0a628871ed60
Joey Lopez <joeyl509625@gmail.com>
2020-09-30 21:16:07 -0500

    finished homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,42 @@
 import React from "react";
-import { Jumbotron } from "react-bootstrap";
+import text from "../assets/text.png";
+import hero from "../assets/feature-2.jpg";
 
 function Home() {
   return (
-    <div>
-      <Jumbotron fluid>
-        <h1>This is the Home Page</h1>
-      </Jumbotron>
+    <div className="container">
+      <div className="navbar">
+        <img src={text} className="logo" alt="" />
+        <nav>
+          <ul>
+            <li>
+              <a href="#DayPass">Day Pass</a>
+            </li>
+            <li>
+              <a href="#Pricing">Pricing</a>
+            </li>
+            <li>
+              <a href="#Training">Personal Training</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+        <img src="" alt="" />
+      </div>
+      <div className="content">
+        <a href="#Sign Up" className="btn">
+          Sign Up Now
+        </a>
+        <h1>
+          Get to know the best
+          <br />
+          version of yourself
+        </h1>
+        <p>The Journey Starts here</p>
+      </div>
+      <img src={hero} className="feature-img" alt="" />
     </div>
   );
 }
